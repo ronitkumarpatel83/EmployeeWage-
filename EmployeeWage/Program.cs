573068f55dcb6bae967cab5cc2ac95dcf4c110bf
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-23 16:24:50 +0530

    [EmployeeWage] Added : Parttime

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -46,16 +46,48 @@ namespace EmployeeWage
 
             }
             Console.ReadLine();
-
-
-
-
         }
-        static void Main(string[] args)
+        public static void UC3()
         {
-            Program.UC1();
-            Program.UC2();
-            
+            Random random = new Random();
+            int is_fulltime = 1;
+            int is_parttime = 2;
+            int totalWorkingHour = 8;
+            int partTimeWorkingHour = 4;
+            int wagePerHour = 20;
+            int dailyWage = 0;
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == is_fulltime)
+            {
+                Console.WriteLine("Employee is Present");
+                dailyWage = totalWorkingHour * wagePerHour;
+                Console.WriteLine("Total wage of a day: " + dailyWage);
+            }
+            else if (empCheck == is_parttime)
+            {
+                Console.WriteLine("Employee is parttime worker");
+                dailyWage = (partTimeWorkingHour * wagePerHour);
+                Console.WriteLine("Total wage of halfday: " + dailyWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                dailyWage = totalWorkingHour * 0;
+                Console.WriteLine("Total wage of a day: " + dailyWage);
+
+            }
+            Console.ReadLine();
+
+
         }
+
+
+            static void Main(string[] args)
+            {
+                Program.UC1();
+                Program.UC2();
+                Program.UC3();
+            }
     }
 }
