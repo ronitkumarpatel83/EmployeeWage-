0630adf8e974a5199b103e5b36cd9aacc5b0f3dc
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-24 16:55:23 +0530

    [EmployeeWage] Added : 20 working days salary

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -83,9 +83,6 @@ namespace EmployeeWage
 
 
         }
-<<<<<<< HEAD
- 
-=======
         public static void UC4()
         {
             Console.WriteLine("Welcome to UC4");
@@ -117,15 +114,55 @@ namespace EmployeeWage
             Console.ReadLine();
         }
 
+        public static void UC5()
+        {
+            Console.WriteLine("Welcome to UC4");
+
+            int is_fulltime = 1;
+            int is_parttime = 2;
+            int totalWorkingHour = 0, wagePerHour = 20, dailyWage = 0;
+            int totalWorkingDay = 1,workinghour;
+
+            while (totalWorkingDay < 20)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        workinghour = 8;
+                        Console.WriteLine("Employee is Present");
+                        break;
+                    case 2:
+                        workinghour = 4;
+                        Console.WriteLine("Employee is Parttime Worker");
+                        break;
+                    default:
+                        workinghour = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
 
 
->>>>>>> UC-4
+                }
+                totalWorkingDay++;
+                totalWorkingHour = totalWorkingHour + workinghour;
+
+            }
+            dailyWage = wagePerHour * totalWorkingHour;
+
+            Console.WriteLine("Total wage is : " + dailyWage);
+            Console.ReadLine();
+        }
+
+
+
         static void Main(string[] args)
             {
                 Program.UC1();
                 Program.UC2();
                 Program.UC3();
                 Program.UC4();
+                Program.UC5();
             }
 
     }
