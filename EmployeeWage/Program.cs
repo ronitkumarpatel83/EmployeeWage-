b133b0f99ba9a0ee71fda2f374a19737c4a81a69
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-03-24 19:33:13 +0530

    [EmployeeWage] Added : 20 working and 100 Hour

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -116,7 +116,7 @@ namespace EmployeeWage
 
         public static void UC5()
         {
-            Console.WriteLine("Welcome to UC4");
+            Console.WriteLine("Welcome to UC5");
 
             int is_fulltime = 1;
             int is_parttime = 2;
@@ -150,10 +150,48 @@ namespace EmployeeWage
             }
             dailyWage = wagePerHour * totalWorkingHour;
 
-            //Console.WriteLine("Total wage is : " + dailyWage);
             Console.WriteLine("Total wage is : " + dailyWage);
+            Console.ReadLine(); 
+        }
+        public static void UC6()
+        {
+            Console.WriteLine("Welcome to UC6");
+            int isFullTime = 1;
+            int isPartTime = 2;
+            int totalWorkingday = 0, empratePerHour = 20, totalhour = 0;
+            int num_of_workingDay = 20, max_Hour_In_a_month = 100,empHrs = 0;
+            int totalSalary = 0;
+
+            while (totalhour <= max_Hour_In_a_month && totalWorkingday < num_of_workingDay)
+
+            {
+                totalWorkingday++;
+                Random random = new Random();
+                int randomCheck = random.Next(0, 3);
+
+                switch (randomCheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is Present");
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is doing parttime:");
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                totalhour += empHrs;
+                Console.WriteLine("Day :" + totalWorkingday + "Daily worked hour", +empHrs);
+            }
+            totalSalary = totalhour * empratePerHour;
+            Console.WriteLine("Total Employee wage is: " + totalSalary );
             Console.ReadLine();
         }
+
 
         static void Main(string[] args)
             {
@@ -162,6 +200,7 @@ namespace EmployeeWage
                 Program.UC3();
                 Program.UC4();
                 Program.UC5();
+                Program.UC6();
             }
 
     }
