64c552251ea9dcdf29054a12bb403037392a254e
Ronit <73175900+ronitkumarpatel83@users.noreply.github.com>
2022-03-30 16:47:57 +0530

    Update Program.cs

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -9,38 +9,7 @@ namespace EmployeeWage
     internal class Program
     {
         static void Main(string[] args)
-<<<<<<< HEAD
-            {
-                Program.UC1();
-                Program.UC2();
-                Program.UC3();
-                Program.UC4();
-                Program.UC5();
-                Program.UC6();
-                Program.UC7();
-              
 
-            //UC8
-            // UC8 main  method
-            Console.WriteLine("Hello, Welcome to total employee wage calculation program ");
-            Console.WriteLine("Please enter the number of Company: ");
-            int limit = int.Parse(Console.ReadLine()), i = 0;
-            while (i < limit)
-            {
-                i++;
-                Console.WriteLine("\nEnter Company Name: ");
-                string com = Console.ReadLine();
-                Console.WriteLine("Enter Employee rate per hour: ");
-                int sal = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter Employee total working hour Limit: ");
-                int wlimt = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter TotalWorking days limit for your company: ");
-                int days = int.Parse(Console.ReadLine());
-                Program.EmployeeWage(com, sal, wlimt, days);
-
-            }
-        }
-=======
         {
             Console.WriteLine("Hello, Welcome to Employeewages problem ");
             WageBuilder obj = new WageBuilder("Dmart", 20, 100, 20);
@@ -49,7 +18,6 @@ namespace EmployeeWage
             WageBuilder obj1 = new WageBuilder("RMart", 10, 120, 21);
             obj1.ComputeEmployeeWage();
             Console.WriteLine(obj1.toString());
->>>>>>> UC-9WageBuilder
 
         }
     }
